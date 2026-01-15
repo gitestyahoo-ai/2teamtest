18fcfb2a1abb487e591dab0b0b296eef7f05fe06
yahoo-afk <sjffhdfg@gmail.com>
2026-01-15 11:39:11 +0900

    test2
    어

---
+++ Source/Test33/Test33.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Test33.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test33, "Test33" );
